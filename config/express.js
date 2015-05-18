c7277ed5889a800595b057a9493a70c34c089eec
Eugenio Mendoza Robaina <eu.mendoza@gmail.com>
2015-05-18 13:26:23 +0200

    Segunda aproximación de la configuración del servidor en express

--- config/express.js
+++ config/express.js
@@ -1,8 +1,24 @@
-var express = require('express');
+var express = require('express'),
+morgan = require('morgan'),
+compression = require('compression'),
+bodyParser = require('body-parser'),
+methodOverride = require('method-override');
 
 module.exports = function() {
 	//creamos la app express
 	var app = express ();
+
+	//configuramos la app
+	if (process.env.NODE_ENV === 'development'){
+		app.use(morgan('dev'));
+	}else if (process.env.NODE_ENV === 'production'){
+		app.use(compression());
+	};
+
+	app.use(bodyParser.urlencoded({extended:true}));
+	app.use(bodyParser.json());
+	app.use(methodOverride());
+
 	//le pasamos la app como parametro al require
 	require('../app/routes/index.server.routes.js')(app);
 	return app;
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+//forzamos a que pille development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var express = require('./config/express.js');
 
 var app = express();
